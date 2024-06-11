ba028397a2e350686a310ade8ae3baa265771041
YannosK <giannos2405@gmail.com>
2024-06-11 17:41:45 +0300

    added a cmake

---
+++ build/CMakeFiles/coordinate_systems.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/coordinate_systems.dir/coordinates.cpp.o"
+  "CMakeFiles/coordinate_systems.dir/coordinates.cpp.o.d"
+  "CMakeFiles/coordinate_systems.dir/main.cpp.o"
+  "CMakeFiles/coordinate_systems.dir/main.cpp.o.d"
+  "coordinate_systems"
+  "coordinate_systems.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/coordinate_systems.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/coordinate_systems.dir/coordinates.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/coordinate_systems.dir/coordinates.cpp.o: \
+ /mnt/ssd_1TB/Yannos/FILES/PROGRAMS/C(++)/Control-and-Computing-MSc/Introduction_to_Cpp/Project/coordinates.cpp \
+ /usr/include/stdc-predef.h \
+ /mnt/ssd_1TB/Yannos/FILES/PROGRAMS/C(++)/Control-and-Computing-MSc/Introduction_to_Cpp/Project/coordinates.hpp
